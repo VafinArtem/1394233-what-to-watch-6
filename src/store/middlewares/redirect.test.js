9009515f72c89881f3ee7d41297a62d8c6cb1192
Vafin Artem <a.vafin85@gmail.com>
2021-03-21 21:50:46 +0500

    Исправил ошибку eslint

--- src/store/middlewares/redirect.test.js
+++ src/store/middlewares/redirect.test.js
@@ -1,6 +1,6 @@
 import {redirect} from './redirect';
 import {redirectToRoute} from '../action';
-import { Url } from '../../consts';
+import {Url} from '../../consts';
 
 const mockRedux = () => {
   const store = {
