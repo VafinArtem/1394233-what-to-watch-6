fc634bcc6ad84f56448384882115a0056304a93a
Vafin Artem <a.vafin85@gmail.com>
2021-03-22 22:50:25 +0500

    сделал карточку фильма ссылкой

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import VideoPreview from '../video-preview/video-preview';
 import {connect} from 'react-redux';
 import {getFilmGenre, getFilmName, resetAmountShowFilms} from '../../store/action';
 
 const MovieCard = ({title, poster, id, genre, previewVideoLink, resetShowFilmsAmount, getName, getGenre}) => {
+  const history = useHistory();
 
   const [isActive, setIsActive] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -24,16 +25,18 @@ const MovieCard = ({title, poster, id, genre, previewVideoLink, resetShowFilmsAm
       onMouseEnter={() => {
         onMouseEnterMovieCard();
       }}
-      onMouseLeave={() => onMouseLeaveMovieCard()}>
+      onMouseLeave={() => onMouseLeaveMovieCard()}
+      onClick={() => {
+        resetShowFilmsAmount();
+        getName(title);
+        getGenre(genre);
+        history.push(`/films/${id}`);
+      }}>
       <div className="small-movie-card__image">
         {isActive ? <VideoPreview poster={poster} url={previewVideoLink} isPlaying={isPlaying} setIsPlaying={setIsPlaying} /> : <img src={poster} alt={title} width={280} height={175} />}
       </div>
       <h3 className="small-movie-card__title">
-        <Link className="small-movie-card__link" to={`/films/${id}`} onClick={() => {
-          resetShowFilmsAmount();
-          getName(title);
-          getGenre(genre);
-        }}>{title}</Link>
+        <Link className="small-movie-card__link" to={`/films/${id}`}>{title}</Link>
       </h3>
     </article>
   );
